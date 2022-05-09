6885462ea9d32c7b9639eff2f04a7ff37a2389de
rlaguswhd19 <rlaguswhd19@naver.com>
2022-05-09 11:57:48 +0900

    Feat : Fetch

--- spring_jpa/src/main/java/com/hj/spring_jpa/jpa/cascade/CascadeRunner.java
+++ spring_jpa/src/main/java/com/hj/spring_jpa/jpa/cascade/CascadeRunner.java
@@ -7,6 +7,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.Set;
 
 @Component
 @Transactional // 하나의 트랜잭션으로 실행 = method에 붙일수도 있고 class에 붙여서 모두 적용할 수 있음
@@ -17,23 +18,32 @@ public class CascadeRunner implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        Post post = new Post();
-        post.setTitle("Spring JPA 공부하기");
+//        Post post = new Post();
+//        post.setTitle("Spring JPA 공부하기");
+//
+//        Comment comment = new Comment();
+//        comment.setComment("회사에서 보기");
+//
+//        post.addComment(comment);
+//
+//        Comment comment1 = new Comment();
+//        comment1.setComment("봐도 되려낭");
+//
+//        post.addComment(comment1);
 
-        Comment comment = new Comment();
-        comment.setComment("회사에서 보기");
+        Post load = entityManager.getReference(Post.class, 1l);
+//        entityManager.persist(post);
 
-        post.addComment(comment);
+        System.out.println(load.getTitle());
 
-        Comment comment1 = new Comment();
-        comment1.setComment("봐도 되려낭");
+        load.getComments().forEach(c -> {
+            System.out.println(c.getComment());
+        });
+//        ManyToOne fetch eager 쿼리를 날릴때 join으로 가져오기 '지금'
+//        OneToMany fetch lazy 쿼리를 날릴때 join으로 가져오지 않고 foreignkey로 가져와서 '나중에' 필요할때 조회
+//        Comment loadComment = entityManager.getReference(Comment.class, 2l);
+//        System.out.println(loadComment.getPost().getTitle());
 
-        post.addComment(comment1);
-
-        entityManager.persist(post);
-
-        Post load = entityManager.getReference(Post.class, post.getId());
-
-        entityManager.remove(load);
+//        entityManager.remove(load);
     }
 }
