77c3c4b4f0013db0d3c9086d334ddc80d51b59fe
rlaguswhd19 <rlaguswhd19@naver.com>
2022-05-09 13:47:58 +0900

    Feat : 스프링 데이터 JPA 원리

---
+++ spring_jpa/src/main/java/com/hj/spring_jpa/jpa/JpaRunner.java
@@ -0,0 +1,21 @@
+package com.hj.spring_jpa.jpa;
+
+import com.hj.spring_jpa.jpa.cascade.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+@Transactional
+public class JpaRunner implements ApplicationRunner {
+
+    @Autowired
+    PostRepository postRepository;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        postRepository.findAll().forEach(System.out::println);
+    }
+}
---
+++ spring_jpa/src/main/java/com/hj/spring_jpa/jpa/cascade/PostRepository.java
@@ -0,0 +1,7 @@
+package com.hj.spring_jpa.jpa.cascade;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+
+}
