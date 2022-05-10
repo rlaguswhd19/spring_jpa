2ac2fe40d6e80245b1b2fb49e2724b40ed4bfbc7
rlaguswhd19 <rlaguswhd19@naver.com>
2022-05-10 17:45:55 +0900

    Feat : Null 처리

--- spring_jpa/src/test/java/com/hj/spring_jpa/repository/CommentRepositoryTest.java
+++ spring_jpa/src/test/java/com/hj/spring_jpa/repository/CommentRepositoryTest.java
@@ -2,11 +2,12 @@ package com.hj.spring_jpa.repository;
 
 import com.hj.spring_jpa.jpa.cascade.Comment;
 import com.hj.spring_jpa.jpa.cascade.repository.CommentRepository;
+import org.assertj.core.internal.bytebuddy.implementation.bytecode.Throw;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-import java.util.List;
+import java.util.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -18,6 +19,7 @@ public class CommentRepositoryTest {
 
     @Test
     public void crud() {
+
         Comment comment = new Comment();
         comment.setComment("test comment");
 
@@ -25,9 +27,16 @@ public class CommentRepositoryTest {
 
         List<Comment> comments = commentRepository.findAll();
         assertThat(comments.size()).isEqualTo(1);
+        assertThat(comments).isNotEmpty();
 
         long count = commentRepository.count();
         assertThat(count).isEqualTo(1);
+
+        Optional<Comment> byId = commentRepository.findById(100l);
+        assertThat(byId).isEmpty();
+//        byId.orElseThrow(IllegalArgumentException::new);
+
+//        commentRepository.save(null);
 
     }
 }
